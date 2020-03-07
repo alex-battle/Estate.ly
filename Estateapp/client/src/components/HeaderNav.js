7ccfdfbc1269467e6be5cf2a7529fd89596dac2f
Alex Battle <58659536+alex-battle@users.noreply.github.com>
2020-03-07 15:49:15 -0500

    routes set

--- Estateapp/client/src/components/HeaderNav.js
+++ Estateapp/client/src/components/HeaderNav.js
@@ -4,7 +4,20 @@ import {NavLink} from 'react-router-dom'
 function HeaderNav(){
     return(
     <div>
-       
+        <div className="leftNav">
+            <NavLink to="/sale">Buy Luxury Properties</NavLink>
+            <NavLink to="/search">Search All Properties</NavLink>
+            <NavLink to="/rent">Luxury Rental Properties</NavLink>
+        </div>
+        <div className="centerNav">
+        <NavLink to ="/">Home</NavLink>
+        </div>
+        <div className="rightNav">
+            <NavLink to="/about">About Us</NavLink>
+            <NavLink to="/staff">Team</NavLink>
+            <NavLink to="/openhouse">Open House</NavLink>
+            <NavLink to="/contact">Contact Us</NavLink>
+        </div>
     </div>
     )
 }
